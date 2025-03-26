0912042957500e1cb707d9f88eac53c0a9b36cb7
punit1407 <punit117aws@gmail.com>
2025-03-27 00:02:25 +0530

    update code

--- Prog03.java
+++ Prog03.java
@@ -7,7 +7,7 @@
 		System.out.println("a+2= "+a+2); //102
 		System.out.println("a+2= "+(a+2)); //12
 		System.out.println("a*5= "+a*5); //50
-		System.out.println("a-6" + a - 6);//compiler error
+		//System.out.println("a-6" + a - 6);//compiler error
 		//bad operands , required string found int
 		System.out.println("a/2= "+a/2); //5
 	}
